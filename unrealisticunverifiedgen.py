9594b746163231c427c46f4ba84823b95a4183d5
ZexFR <90112340+ZexFR@users.noreply.github.com>
2022-04-28 07:49:09 +0100

    Update unrealisticunverifiedgen.py

--- unrealisticunverifiedgen.py
+++ unrealisticunverifiedgen.py
@@ -1,11 +1,11 @@
-import os
+import os                                                         
 from dhooks.client import Webhook
 from discord_webhook import DiscordWebhook
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options                            
 import time
 from datetime import date, datetime
 import json
@@ -22,53 +22,53 @@ import pyautogui
 
 init()
 
+# Sorry for the auto auth not working ATM. This is because Discord changed their systems the last time I did this. Thanks
 
 
-
-def random_username():
- with open("MyFile.txt", "r") as file:
+def random_username():         # defines the random username gen    
+ with open("MyFile.txt", "r") as file:                  
   allText = file.read()
   words = list(map(str, allText.split()))
 
 
 
 
-def get_random_string(length):
+def get_random_string(length):       # defines the random string to make the email etc
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
 
 
 def runwindow():
-    username = str(input(Fore.BLUE + "Write what name you want » "+ Fore.RESET))
-    password = str(input(Fore.GREEN + "Write what password you want » " + Fore.RESET))
+    username = str(input(Fore.BLUE + "Write what name you want » "+ Fore.RESET)) # asks what username you want
+    password = str(input(Fore.GREEN + "Write what password you want » " + Fore.RESET)) # asks what password you want
     while True:
-        proxy = random.choice(open("proxies.txt").readlines())
-        print(Fore.GREEN + 'Using proxy ' + proxy + '.')
-        driver = webdriver.Chrome(executable_path='chromedriver.exe')
-        options = webdriver.ChromeOptions()
-        options.add_argument("--incognito")
-        options.add_argument("start-maximized")
-        options.add_argument('--proxy-server=%s' % proxy)
-        driver = webdriver.Chrome(chrome_options=options)
-        driver.get("https://discord.com/register")
+        proxy = random.choice(open("proxies.txt").readlines()) # opens the proxies file to gather the proxies
+        print(Fore.GREEN + 'Using proxy ' + proxy + '.') # prints which proxy it's using
+        driver = webdriver.Chrome(executable_path='chromedriver.exe') # opens chromedriver
+        options = webdriver.ChromeOptions() 
+        options.add_argument("--incognito") # turns on incognito mode
+        options.add_argument("start-maximized") # minimises the screen
+        options.add_argument('--proxy-server=%s' % proxy) # starts the proxy server
+        driver = webdriver.Chrome(chrome_options=options) 
+        driver.get("https://discord.com/register") # Opens Discord Register System
         time.sleep(1)
-        email = get_random_string(8) + str(random.randint(1234, 9876)) + "@gmail.com"
-        user = get_random_string(6 ) + str(random.randint(1234, 9876)) + " | Zex"
-        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div/div/form/div/div/div[1]/div/input').send_keys(
-            email)
+        email = get_random_string(8) + str(random.randint(1234, 9876)) + "@gmail.com" # Creates a random email with @gmail.com at the end
+        user = get_random_string(6 ) + str(random.randint(1234, 9876)) + " | Zex" # Creates a random amount of letters/numbers for the username 
+        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div/div/form/div/div/div[1]/div/input').send_keys(        #Writes the email in the box
+            email) 
         time.sleep(0.1)
-        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div/div/form/div/div/div[2]/div/input').send_keys(get_random_string(3) + str(random.randint(1234, 9876)) +  " | " + username)
+        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div/div/form/div/div/div[2]/div/input').send_keys(get_random_string(3) + str(random.randint(1234, 9876)) +  " | " + username) # Writes the username in the box
         time.sleep(0.1)
-        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div/div/form/div/div/div[3]/div/input').send_keys(
-            password)
+        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div/div/form/div/div/div[3]/div/input').send_keys(    # Writes the password in the box
+            password) 
         time.sleep(0.1)
         driver.find_element_by_xpath(
-            '//*[@id="app-mount"]/div[2]/div/div/div/form/div/div/div[4]/div[1]/div[1]/div/div/div/div/div[1]').click()
+            '//*[@id="app-mount"]/div[2]/div/div/div/form/div/div/div[4]/div[1]/div[1]/div/div/div/div/div[1]').click()  # Verifies the button
         time.sleep(0.3)
         actions = ActionChains(driver)
         time.sleep(.2)
-        driver.find_elements_by_class_name('css-1hwfws3')[0].click()
+        driver.find_elements_by_class_name('css-1hwfws3')[0].click() # Line 71 - 80 is the date writing
         actions.send_keys(str(random.randint(1, 12)))
         actions.send_keys(Keys.ENTER)
         actions.send_keys(str(random.randint(1, 28)))
@@ -84,22 +84,22 @@ def runwindow():
         time.sleep(7)
         os.system("cls")
         print("Please do the Auth")
-        input("Press Enter to when you have done the puzzle :)")
+        input("Press Enter to when you have done the puzzle :)") # Waits for you to do the auth
         time.sleep(3)
         driver.switch_to.default_content()
         print(Fore.RESET + ' ')
         time.sleep(3)
  
         token = driver.execute_script(
-            "let popup; popup = window.open('', '', `width=1,height=1`); if(!popup || !popup.document || !popup.document.write) console.log('Please allow popups'); window.dispatchEvent(new Event('beforeunload')); token = popup.localStorage.token.slice(1, -1); popup.close(); return token")
+            "let popup; popup = window.open('', '', `width=1,height=1`); if(!popup || !popup.document || !popup.document.write) console.log('Please allow popups'); window.dispatchEvent(new Event('beforeunload')); token = popup.localStorage.token.slice(1, -1); popup.close(); return token") # Gets the token of the account
 
-        hook = "Enter your webhook here or delete line: 97 - 101"
+        hook = "Enter your webhook here or delete line: 97 - 101" 
         r = requests.post(hook, json={
             "content": token
         })
-        print(f'Sent webhook post request. Status = {r.status_code} ~ {r.reason}')
+        print(f'Sent webhook post request. Status = {r.status_code} ~ {r.reason}') # Sends the token to your specified webhook
         print(f"{token}")
-        with open('tokens.txt', 'a') as f:
+        with open('tokens.txt', 'a') as f:   # Writes the token in the folder "tokens.txt"
             f.write(token)
             f.write('\n')
         driver.quit()
